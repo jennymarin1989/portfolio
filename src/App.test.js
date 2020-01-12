a9bf9d8bb1dd2d3befcc008730ffc89f2e2adfff
Jenny <jennymarin1989@hotmail.com>
2020-01-12 22:38:24 +0000

    Use create-react-up to start the project and wrote firts test for App.js

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { shallow } from 'enzyme';
 import App from './App';
 import Home from '../src/containers/Home';
+import AboutMe from '../src/containers/AboutMe';
 
 describe('Renders home page correctly', () => {
   it('renders without crashing', () => {
@@ -14,6 +15,11 @@ describe('Renders home page correctly', () => {
   it('renders the homepage component correctly', () => {
     const homepage = shallow(<Home />)
     expect(homepage).toBeTruthy()
+  });
+
+  it('renders the homepage component correctly', () => {
+    const aboutMePage = shallow(<AboutMe />)
+    expect(aboutMePage).toBeTruthy()
   });
 
 })
