0f4642bfe1ba057a10ceb56bd96faf2e463be87b
Jenny <jennymarin1989@hotmail.com>
2020-01-12 22:39:11 +0000

    Update App.js component and pass test

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React, { Fragment } from 'react';
 import Home from '../src/containers/Home'
-import './App.scss';
+import AboutMe from '../src/containers/AboutMe'
+
+import '../src/styles/base.scss';
 
 const App = () => {
   return (
     <Fragment>
       <div className="App">
-        <Home />
+        <section>
+          <Home tag="h1" className="homepage" text="Welcome" />
+        </section>
+        <section>
+          <AboutMe tag="h1" className="about-me" text="About me" />
+        </section>
       </div>
     </Fragment>
   );
