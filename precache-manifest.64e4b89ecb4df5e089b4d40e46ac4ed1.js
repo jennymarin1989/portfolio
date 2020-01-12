08d0f8fea53d4c20a659ce4deedb8be60f86e054
Jenny <jennymarin1989@hotmail.com>
2020-01-12 22:59:14 +0000

    Updates

---
+++ precache-manifest.64e4b89ecb4df5e089b4d40e46ac4ed1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dece5ab0811a8fc996b8062bfade552f",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "fd227d284e86c71e08ee",
+    "url": "/portfolio/static/css/main.a3ad6cb3.chunk.css"
+  },
+  {
+    "revision": "34c83c2c59a3eeb734f5",
+    "url": "/portfolio/static/js/2.4865e374.chunk.js"
+  },
+  {
+    "revision": "fd227d284e86c71e08ee",
+    "url": "/portfolio/static/js/main.3fa0c854.chunk.js"
+  },
+  {
+    "revision": "2230669ff420ab891080",
+    "url": "/portfolio/static/js/runtime-main.7e82ed09.js"
+  },
+  {
+    "revision": "98bfbe051c81822d6e762713956d38b1",
+    "url": "/portfolio/static/media/black-pen-arrow.98bfbe05.svg"
+  },
+  {
+    "revision": "d7c0c37d99cb4afe8bd2f8c9acba4eed",
+    "url": "/portfolio/static/media/green-pen-arrow.d7c0c37d.svg"
+  },
+  {
+    "revision": "28bf1e6293c30909c16197603dfde91f",
+    "url": "/portfolio/static/media/purple-pen-arrow.28bf1e62.svg"
+  }
+]);
