33f8abb8e41436ce48279f9bab0e37e142eda07d
Jenny <jennymarin1989@hotmail.com>
2020-01-12 22:40:59 +0000

    Create Header component and pass failing test

--- src/components/atoms/Header/index.js
+++ src/components/atoms/Header/index.js
@@ -1,12 +1,22 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 
-const Header = ({ tag: Tag }) => {
+const Header = ({ tag: Tag, className, text }) => {
   return (
     <div>
-      <Tag>My Portfolio!</Tag>
+      <Tag className={className}>
+        {text}
+      </Tag>
     </div>
   )
 }
 
+
 export default Header
+
+Header.propTypes = {
+  tag: PropTypes.oneOf(['h1', 'h2', 'h3']),
+  className: PropTypes.string,
+  text: PropTypes.string.isRequired
+}
