1069d3d0cd9b14a86752a98c9ccdc6dbc30a240c
Jenny <jennymarin1989@hotmail.com>
2020-01-12 22:40:27 +0000

    Create test for Header component

--- src/__tests__/Header.test.js
+++ src/__tests__/Header.test.js
@@ -1,14 +1,34 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import { shallow, mount } from 'enzyme';
 import Header from '../components/atoms/Header'
 
 
-describe('Renders header component correctly', () => {
+function createTestProps(props) {
+  return {
+    tag: 'h1',
+    className: 'header',
+    text: 'Welcome to my portfolio'
+  }
+}
 
-  it('Renders the tag prop correctly', () => {
-    const wrapper = mount(<Header tag="h1" />)
-    expect(wrapper.props().tag).toEqual("h1")
+
+describe('Renders header props correctly', () => {
+
+  let wrapper;
+  let props;
+
+  beforeEach(() => {
+    props = createTestProps()
+    wrapper = shallow(<Header tag={props.tag} text={props.text} className="header" />);
   })
 
+  it('Renders the tag prop correctly', () => {
+    const value = wrapper.find('h1').length
+    expect(value).toEqual(1)
+  })
+
+  it('Renders the class prop correctly', () => {
+    const value = wrapper.find('.header').length
+    expect(value).toEqual(1)
+  })
 })
