2d3cd39b471e9940740637d9a5419ce098488aea
Jenny <jennymarin1989@hotmail.com>
2020-01-17 17:09:15 +0000

    Update component adding the SvgBackGround component

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,34 +1,47 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Header from '../../components/atoms/Header'
+import SvgBackGround from '../../components/molecules/SvgBackGround'
 import blackArrow from '../../images/black-pen-arrow.svg'
-
+import greenArrow from '../../images/green-pen-arrow.svg'
+import purpleArrow from '../../images/purple-pen-arrow.svg'
 
 import '../../lib/mouseMove'
 import './index.scss'
 
+let timer = null
+
 const Home = ({ tag, className, text }) => {
+  let imageArray = [blackArrow, greenArrow, purpleArrow]
+  let count = 0
+
+  const changeColor = () => {
+    count = count + 1
+    count = count % imageArray.length
+    let image = imageArray[count]
+
+    let images = document.querySelectorAll('img')
+
+    Array.from(images).map(img => {
+      return img.setAttribute('src', image)
+    })
+  }
+
+  useEffect(() => {
+    timer = setInterval(() => changeColor(), 3000)
+    return function cleanUp() {
+      clearInterval(timer)
+    }
+  })
+
   return (
     <div>
-      <div className='homepage'>
-
-        <img src={blackArrow} className='homepage__img-arrow' alt=' arrow' style={{ top: '6%', left: '2%' }} />
-        <img src={blackArrow} className='homepage__img-arrow' alt=' arrow' style={{ top: '20%', left: '18%' }} />
-        <img src={blackArrow} className='homepage__img-arrow' alt=' arrow' style={{ top: '45% ', left: '4%' }} />
-        <img src={blackArrow} className='homepage__img-arrow' alt=' arrow' style={{ top: '80%', left: '2%' }} />
-        <img src={blackArrow} className='homepage__img-arrow' alt=' arrow' style={{ top: '6%', left: '83%' }} />
-        <img src={blackArrow} className='homepage__img-arrow' alt=' arrow' style={{ top: '45%', left: '80%' }} />
-        <img src={blackArrow} className='homepage__img-arrow' alt=' arrow' style={{ top: '83%', left: '83%' }} />
-        <img src={blackArrow} className='homepage__img-arrow' alt=' arrow' style={{ top: '75%', left: '40%' }} />
-        <img src={blackArrow} className='homepage__img-arrow' alt=' arrow' style={{ top: '15%', left: '40%' }} />
-        <img src={blackArrow} className='homepage__img-arrow' alt=' arrow' style={{ top: '35%', left: '70%' }} />
-        <img src={blackArrow} className='homepage__img-arrow' alt=' arrow' style={{ top: '71%', left: '17%' }} />
-        <img src={blackArrow} className='homepage__img-arrow' alt=' arrow' style={{ top: '51%', left: '18%' }} />
-        <img src={blackArrow} className='homepage__img-arrow' alt=' arrow' style={{ top: '12%', left: '60%' }} />
-        <img src={blackArrow} className='homepage__img-arrow' alt=' arrow' style={{ top: '70%', left: '57%' }} />
-
-        <Header tag='h1' className="homepage__header" text='Welcome to my Portfolio ;)' />
+      <div>
+        <SvgBackGround backgroundImg={blackArrow} />
       </div>
-    </div >
+      <div className="homepage">
+        <Header tag="h1" className="homepage__header" text="Welcome to my Portfolio ;)" />
+      </div>
+    </div>
   )
 }
 
