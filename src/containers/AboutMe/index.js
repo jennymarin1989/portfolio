a368ba1d1cd0627616d038456228667700916378
Jenny <jennymarin1989@hotmail.com>
2020-01-17 17:11:35 +0000

    Add changes after deploying to git pages

--- src/containers/AboutMe/index.js
+++ src/containers/AboutMe/index.js
@@ -7,7 +7,7 @@ import './index.scss'
 const AboutMe = () => {
   return (
     <div className="about-me">
-      <Header tag="h1" text="What about me ...?" />
+      <Header tag="h1" text="What about me ...? ( Under construction )" />
     </div>
   )
 }
---
+++ src/precache-manifest.6a2e8aebe615eac86456570f6b272936.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b91035eecd3192121ec1bbb84ecd4fe8",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "965eba9c13a9544f2710",
+    "url": "/portfolio/static/css/main.a3ad6cb3.chunk.css"
+  },
+  {
+    "revision": "34c83c2c59a3eeb734f5",
+    "url": "/portfolio/static/js/2.4865e374.chunk.js"
+  },
+  {
+    "revision": "965eba9c13a9544f2710",
+    "url": "/portfolio/static/js/main.8b0c1dc6.chunk.js"
+  },
+  {
+    "revision": "2230669ff420ab891080",
+    "url": "/portfolio/static/js/runtime-main.7e82ed09.js"
+  },
+  {
+    "revision": "98bfbe051c81822d6e762713956d38b1",
+    "url": "/portfolio/static/media/black-pen-arrow.98bfbe05.svg"
+  },
+  {
+    "revision": "d7c0c37d99cb4afe8bd2f8c9acba4eed",
+    "url": "/portfolio/static/media/green-pen-arrow.d7c0c37d.svg"
+  },
+  {
+    "revision": "28bf1e6293c30909c16197603dfde91f",
+    "url": "/portfolio/static/media/purple-pen-arrow.28bf1e62.svg"
+  }
+]);
