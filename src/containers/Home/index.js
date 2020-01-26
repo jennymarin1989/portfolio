6f08464bcb71dd448be4034440097c7e50fc938d
Jenny <jennymarin1989@hotmail.com>
2020-01-26 21:41:43 +0000

    Refactor Home component isolating logic into a customed hook

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -2,41 +2,17 @@ import React, { useEffect } from 'react'
 import Header from '../../components/atoms/Header'
 import SvgBackGround from '../../components/molecules/SvgBackGround'
 import blackArrow from '../../images/black-pen-arrow.svg'
-import greenArrow from '../../images/green-pen-arrow.svg'
-import purpleArrow from '../../images/purple-pen-arrow.svg'
-
+import { useModifyArrows } from '../../hooks/useModifyArrows'
 import '../../lib/mouseMove'
 import './index.scss'
 
-let timer = null
-
 const Home = ({ tag, className, text }) => {
-  let imageArray = [blackArrow, greenArrow, purpleArrow]
-  let count = 0
-
-  const changeColor = () => {
-    count = count + 1
-    count = count % imageArray.length
-    let image = imageArray[count]
-
-    let images = document.querySelectorAll('img')
-
-    Array.from(images).map(img => {
-      return img.setAttribute('src', image)
-    })
-  }
-
-  useEffect(() => {
-    timer = setInterval(() => changeColor(), 3000)
-    return function cleanUp() {
-      clearInterval(timer)
-    }
-  })
+  const { image } = useModifyArrows()
 
   return (
     <div>
       <div>
-        <SvgBackGround backgroundImg={blackArrow} />
+        <SvgBackGround backgroundImg={image} />
       </div>
       <div className="homepage">
         <Header tag="h1" className="homepage__header" text="Welcome to my Portfolio ;)" />
